9e3d7869c479eec570c3ff43dba3c6b58c2a819a
ceomlc <>
2026-03-24 20:33:25 -0400

    Fix bcrypt import: use bcryptjs which is the installed package

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { db } from "./db";
 import { users } from "../shared/schema";
 import { eq } from "drizzle-orm";
